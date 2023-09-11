622da6d61252e603aa38d912a53693d1e7d3f771
seantaylor1006 <seant1006@gmail.com>
2023-09-11 04:34:43 -0400

    inquirer prompt worked on, shapes classes

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+inquirer.createPromptModule([
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'select a shape:',
+        choices: ['Circle', 'Square', 'Triangle']
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'select a color:'
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'type the 3 letters of your logo:'
+    }
+]).then((Response) => {
+    console.log('creating logo');
+    fs.writeFileSync('logo.svc', constructLogo(Response)), err => {
+        err ? console.err(err) : console.log('Logo created')
+    }
+});
